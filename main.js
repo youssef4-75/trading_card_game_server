fe98352d8d1ab07e82acc9886bf140a5e7eda2f6
haddadi <youssefhaddadi115@gmail.com>
2025-01-24 22:07:37 +0100

    deleting the cors module

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 // a server code, made to deal with the front end part of our project in the web class
 import express from 'express';
-import cors from 'cors';
 import { lookForAuthentication, saveProfile, SendingProfileData as sendingProfileData } from './core/profiles.js';
 import { acceptTrade, addNewTrades, sendAllTradesToUser } from './core/trades.js';
 import { getInventory } from './core/inventory.js';
 import { distribute17 } from './database/database_communication.js';
 
-
 const app = express(); 
 
 const port = 3000;
-
-app.use(cors());
 
 app.use(express.json());
 
